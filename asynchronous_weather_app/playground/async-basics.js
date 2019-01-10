a1376ab6a2a7c8a794000031856b416ede74ce59
adzarei <adzarei@inf.upv.es>
2019-01-10 12:47:26 +0100

    end of playground test example

--- asynchronous_weather_app/playground/async-basics.js
+++ asynchronous_weather_app/playground/async-basics.js
@@ -4,4 +4,8 @@ setTimeout(() => {
     console.log('Inside of callback');
 },200);
 
+setTimeout( () => {
+    console.log('0 seconds timeout...');
+},0);
+
 console.log('finishing async.basics.js...');
